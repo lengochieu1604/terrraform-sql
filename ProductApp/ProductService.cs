53cba0a882eda09372f1e37dca29397322ba31dc
Lê Ngọc Hiếu <98753976+lengochieu1604@users.noreply.github.com>
2022-03-29 20:05:39 +0700

    Update ProductService.cs

--- ProductApp/ProductService.cs
+++ ProductApp/ProductService.cs
@@ -8,7 +8,7 @@ namespace ProductApp
     public class ProductService
     {
         // private static string db_connectionstring = "server=appserver6008089.database.windows.net;user=sqladmin;password=Azure@123;database=appdb";
-        private static string db_connectionstring = "server=appserver7531.database.windows.net;user=sqladmin;password=Azure@123;database=appdb";
+        private static string db_connectionstring = ";user=;password=;database=appdb";
 
         private SqlConnection GetConnection()
         {
